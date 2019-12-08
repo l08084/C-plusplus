0f61cb61aeef80ccc60d885b6e9d1df17379b457
l08084 <l08084.1989@gmail.com>
2019-12-08 17:54:33 +0900

    update

---
+++ chap01/list0107.cpp
@@ -0,0 +1,18 @@
+// 二つの変数xとyの合計と平均値を表示
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+
+    x = 63;
+    y = 18;
+
+    cout << "xの値は" << x << "です。\n";
+    cout << "yの値は" << y << "です。\n";
+
+    cout << "合計は" << x + y << "です。\n";
+    cout << "平均は" << (x + y) / 2 << "です。\n";
+}
