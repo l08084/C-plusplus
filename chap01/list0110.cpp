eb2ea70c17f1b7a61dea7129f1810ebea4edba91
l08084 <l08084.1989@gmail.com>
2019-12-09 17:50:05 +0900

    update

---
+++ chap01/list0110.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+
+    cout << "x,yを加算します。\n";
+
+    cout << "xの値:";
+
+    cin >> x;
+
+    cout << "yの値：";
+
+    cin >> y;
+
+    cout << "x+y:" << x + y << "。\n";
+}
