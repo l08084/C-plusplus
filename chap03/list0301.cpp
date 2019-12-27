65c4b51306c91c3386014d9b050ba5f24e451298
l08084 <l08084.1989@gmail.com>
2019-12-27 21:36:33 +0900

    update

---
+++ chap03/list0301.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string retry;
+
+    do
+    {
+        int month;
+        cout << "季節を求めます。\n何月ですか：";
+        cin >> month;
+
+        if (month >= 3 && month <= 5)
+        {
+            cout << "それは春です。\n";
+        }
+        else if (month >= 6 && month <= 8)
+        {
+            cout << "それは夏です。\n";
+        }
+        else if (month >= 9 && month <= 11)
+        {
+            cout << "それは秋です。\n";
+        }
+        else if (month >= 12 || month == 1 || month == 2)
+        {
+            cout << "それは冬です。\n";
+        }
+        else
+        {
+            cout << "そんな月はありませんよ";
+        }
+
+        cout << "もう一度？ Y・・・YES / N・・・NO：";
+        cin >> retry;
+    } while (retry == "Y" || retry == "y");
+}
